a751aea90f9d8ce02490efd24e2a7c4bfde642b3
Nathaniel Dunham <dun19007@byui.edu>
2022-11-11 15:59:17 -0700

    updated main index

--- site-plan/scripts/script.js
+++ site-plan/scripts/script.js
@@ -1,6 +1,12 @@
 
-var games = ['/dnd_page.html','/fallout_page.html'];
-var gameSelected = Math.floor(Math.random() * 1);
+const button = document.querySelector("#take_a_chance_btn");
 
-return games[game]
+function goToPage(){
+    var games = ['/dnd_page.html','/fallout_page.html'];
+    var gameSelected = Math.floor(Math.random() * 1);
+    window.location.href += games[gameSelected];
+    return;
+}
+
+button.addEventListener("click", goToPage);
 
