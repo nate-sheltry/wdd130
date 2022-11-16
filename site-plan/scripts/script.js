2deb2000b31e434eca1c41830ca0efef2b24cb28
Nathaniel Dunham <dun19007@byui.edu>
2022-11-16 14:58:52 -0700

    updated

--- site-plan/scripts/script.js
+++ site-plan/scripts/script.js
@@ -1,12 +1,14 @@
 
-const button = document.querySelector("#take_a_chance_btn");
+if(document.querySelector("#take-a-chance-btn")){
+    const button = document.querySelector("#take-a-chance-btn");
 
-function goToPage(){
-    var games = ['/dnd_page.html','/fallout_page.html'];
-    var gameSelected = Math.floor(Math.random() * 1);
-    window.location.href += games[gameSelected];
-    return;
+    function goToPage(){
+        let games = ['dnd_page.html','fallout_page.html'];
+        let gameSelected = Math.round(Math.random() * 1);
+        window.location.href = games[gameSelected];
+        return;
+    }
+
+    button.addEventListener("click", goToPage);
 }
-
-button.addEventListener("click", goToPage);
 
